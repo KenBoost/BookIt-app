285a8c821a4243867d42e71c41719c6ddec0ab88
Keneth Miranda <kennethmiranda56@gmail.com>
2023-10-10 19:17:09 -0600

    installe el font awesome y todo va bien

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Nav from './components/Navbar';
+import Layout from './components/Layout';
+import Home from './components/Home';
+
 import logo from './logo.svg';
 import './App.scss';
 
